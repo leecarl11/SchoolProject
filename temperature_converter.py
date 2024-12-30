c93c86bc215f0a3e2ee56ced792f4a1b62405e49
leecarl2002 <leecarl11@gmail.com>
2024-12-30 03:08:06 +0000

    Add files via upload

---
+++ temperature_converter.py
@@ -0,0 +1,7 @@
+celsius=32
+fahrenheit=80
+def main():
+    celsius=float(input("Enter temperature in Celsius: "))
+    fahrenheit=9/5*celsius+32
+    print(f"{fahrenheit} degrees fahrenheit")
+main()
